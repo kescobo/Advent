1bb1d032c4f399bf743ca0dcc37234eeccc2286c
Kevin Bonham <kevbonham@gmail.com>
2020-12-01 11:08:09 -0500

    make module

---
+++ 2020/src/Advent2020.jl
@@ -0,0 +1,17 @@
+module Advent2020
+
+export in_to_int,
+       accounting
+
+using Combinatorics
+
+function in_to_int(input)
+    return parse.(Int, input)
+end
+
+function accounting(input, n)
+    for c in combinations(input, n)
+        reduce(+, c) == 2020 && return reduce(*, c)
+    end
+    error("No combinations of $n elements sum to 2020")
+end
