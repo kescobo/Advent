796e1c1b49651a4143810a30a2365684add046b6
Kevin Bonham <kevbonham@gmail.com>
2020-12-02 14:01:28 -0500

    add tests

---
+++ 2020/test/runtests.jl
@@ -0,0 +1,10 @@
+using Test
+using Weave
+
+@testset "Day 1" begin
+    Weave.include_weave("../notebooks/day1.jmd")
+end
+
+@testset "Day 2" begin
+    Weave.include_weave("../notebooks/day2.jmd")
+end
